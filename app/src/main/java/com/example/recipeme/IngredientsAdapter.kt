fc11f5a475c872329d82c0ba5a2575eff696a600
SteveMontelongo <montestev15@gmail.com>
2023-03-25 14:23:02 -0500

    Fixed error, adding functionality to main activity.

--- app/src/main/java/com/example/recipeme/IngredientsAdapter.kt
+++ app/src/main/java/com/example/recipeme/IngredientsAdapter.kt
@@ -10,12 +10,12 @@ import androidx.recyclerview.widget.RecyclerView
 
 class IngredientsAdapter (private val ings: List<Ingredient>): RecyclerView.Adapter<IngredientsAdapter.ViewHolder>(){
     class ViewHolder(iView: View): RecyclerView.ViewHolder(iView){
-        val nameView TextView;
-        val QuantityView: TextView;
+        val nameView: TextView;
+        val quantityView: TextView;
 
         init {
             nameView = iView.findViewById(R.id.tvIngredientNameItem);
-            QuantityView = iView.findViewById(R.id.tvIngredientQuantityItem);
+            quantityView = iView.findViewById(R.id.tvIngredientQuantityItem);
 
         }
     }
@@ -30,7 +30,7 @@ class IngredientsAdapter (private val ings: List<Ingredient>): RecyclerView.Adap
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = ings.get(position)
         holder.nameView.text = item.name;
-        holder.nameView.text = item.quantity;
+        holder.quantityView.text = item.quantity;
     }
 
     override fun getItemCount(): Int {
