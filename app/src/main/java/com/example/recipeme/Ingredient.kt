61f727d2eb41c44a82d898ea0b2704a5bf68bb99
SteveMontelongo <montestev15@gmail.com>
2023-03-25 14:17:46 -0500

    Needs debugging for adapter

---
+++ app/src/main/java/com/example/recipeme/Ingredient.kt
@@ -0,0 +1,4 @@
+package com.example.recipeme
+
+class Ingredient(val name: String, val quantity: String) {
+}
---
+++ app/src/main/java/com/example/recipeme/IngredientsAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.recipeme
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+
+class IngredientsAdapter (private val ings: List<Ingredient>): RecyclerView.Adapter<IngredientsAdapter.ViewHolder>(){
+    class ViewHolder(iView: View): RecyclerView.ViewHolder(iView){
+        val nameView TextView;
+        val QuantityView: TextView;
+
+        init {
+            nameView = iView.findViewById(R.id.tvIngredientNameItem);
+            QuantityView = iView.findViewById(R.id.tvIngredientQuantityItem);
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder{
+        val context = parent.context;
+        val inflater = LayoutInflater.from(context);
+        val wishItemView = inflater.inflate(R.layout.ingredient_item, parent, false)
+        return ViewHolder(wishItemView);
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = ings.get(position)
+        holder.nameView.text = item.name;
+        holder.nameView.text = item.quantity;
+    }
+
+    override fun getItemCount(): Int {
+        return ings.size;
+    }
+
+
+}
